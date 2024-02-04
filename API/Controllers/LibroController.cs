afb5ee11659e62bc71433b8811ff5dd43a8b0fe7
kendra-svg <ksanchov@ucenfotec.ac.cr>
2024-02-04 16:54:22 -0600

    Add project files.

---
+++ API/Controllers/LibroController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class LibroController : ControllerBase
+    {
+
+        Dictionary<int, string> libros = new Dictionary<int, string> {
+            { 1, "Orgullo y Prejuicio"},
+            { 2, "Cartas de amor a los muertos"},
+            { 3, "Harry Potter y el Prisionero de Azkaban"}
+          };
+
+
+        [HttpGet]
+        public String RetrieveAllBooks() {
+
+         /* Dictionary<int, string> libros = new Dictionary<int, string> {
+            { 1, "Orgullo y Prejuicio"},
+            { 2, "Cartas de amor a los muertos"},
+            { 3, "Harry Potter y el Prisionero de Azkaban"} 
+          };*/
+
+            // Extraer solo los nombres de los libros
+            var nombresLibros = libros.Values.ToList();
+
+            // Unir los nombres de los libros en una cadena separada por coma
+            string librosSeparadosPorComa = string.Join(", ", nombresLibros);
+
+            return librosSeparadosPorComa;
+
+        }
+
+        [HttpGet("{id}")]
+        public string RetrieveBook(int id)
+        {
+            if (libros.ContainsKey(id))
+            {
+                return libros[id];
+            }
+            else
+            {
+                return "Libro no encontrado, pruebe con otro número";
+            }
+        }
+
+
+
+    }
+}
---
+++ API/Controllers/PatientsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")] /*con action se refiere al nombre, por ejemplo GetPatient*/
+    /*despues del https://localhost:7176/ viene el URI (uniform resoure identifier) que empieza a partir del api/*/
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetPatient(int id) 
+        {
+            return "Datos del GET";
+        }
+
+        [HttpGet]
+        public string GetAllPatient()
+        {
+            return "Datos del GetAllPatients";
+        }
+
+    }
+}
